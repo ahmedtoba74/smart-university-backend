4461311930e8c1c3d02892103cd5b802e8f8efdf
Ahmed Toba <ahmdtwbt22@gmail.com>
2025-12-01 00:29:26 +0200

    Add Error Handling

--- app.js
+++ app.js
@@ -1,18 +1,70 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
+import sanitize from 'express-mongo-sanitize';
+import AppError from './src/utils/appError.js';
+import globalErrorHandler from './src/utils/errorHandler.js';
 
 // Load env vars
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Basic Middleware
-app.use(express.json());
+// 1- Global Middleware
+
+// Enable CORS
+app.use(cors());
+
+//  Development logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again after 10 minutes'
+});
+
+// Apply rate limiter to all requests
+app.use('/api', limiter);
+
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+// app.use(sanitize());
+
+// Prevent HTTP Parameter Pollution
+app.use(hpp());
+
+// Secure app by setting headers
+app.use(helmet());
+
+// Compress text responses
+app.use(compression());
+
+// Parse cookies
+app.use(cookieParser());
 
 // Test Route
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Smart University Backend is Running' });
 });
+
+// Handle Unhandled Routes
+app.all(/(.*)/, (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 export default app;
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-import express from 'express';
 import dotenv from 'dotenv';
 import dbConnection from './DB/dbConnection.js';
+import app from './app.js';
+
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught Exception! Shutting down...");
+    console.error(err.name, err.message);
+    process.exit(1);
+});
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 3000;
 
 dbConnection();
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled Rejection! Shutting down...");
+    console.error(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
